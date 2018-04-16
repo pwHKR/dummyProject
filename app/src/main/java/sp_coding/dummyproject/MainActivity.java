48b423b3a6725c3ac612e4edf304a7e3bb038772
pwHKR <peter.westin0028@stud.hkr.se>
2018-04-16 21:11:05 +0200

    Student A first Commit

--- app/src/main/java/sp_coding/dummyproject/MainActivity.java
+++ app/src/main/java/sp_coding/dummyproject/MainActivity.java
@@ -2,6 +2,7 @@ package sp_coding.dummyproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    private void hejAndreas(String says){
+
+
+        Log.d("Andreas says: ", says);
+
     }
 }
